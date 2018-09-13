82b094fb12e4a9f60dd8b3516a30d87213fb6c31
Aiswarya Bhaseendran Pillai <aiswaryalb@gmail.com>
2018-09-13 16:13:07 +0530

    Add files via upload

---
+++ PaginationEg/build/web/index.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : index
+    Created on : Sep 13, 2018, 2:46:11 PM
+    Author     : Aiswarya
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <a href="ViewServlet?page=1">View Employees</a>
+    </body>
+</html>
---
+++ PaginationEg/src/java/Emp.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Aiswarya
+ */
+public class Emp {
+    private int id;
+    private String name;
+    private float salary;
+
+    public void setId(int aInt) {
+        id=aInt;
+    }
+
+   public void setName(String string) {
+      name=string;
+    }
+
+   public void setSalary(float aFloat) {
+        salary=aFloat;
+    }
+
+     public int getId() {
+        return id;
+    }
+
+   public String getName() {
+        return name;
+    }
+
+  public float getSalary() {
+        return salary;
+    }
+    
+    
+}
